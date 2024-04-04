fbc31d76aa28380596355068f8c03ad51731f27b
Dmitry Melnikov <d.melnikov.alex@gmail.com>
2024-04-04 20:13:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Домашнее задание к занятию "9. Анимации и CSS"
+
+![CI](https://github.com/alvarez1213/ahj-hw-9/actions/workflows/web.yml/badge.svg)
+
+Сайт развернут [тут](https://alvarez1213.github.io/ahj-hw-9)
---
+++ src/js/app.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector(".btn");
+const collapseMessage = document.querySelector(".collapse-message");
+
+btn.addEventListener("click", () => {
+  if (collapseMessage.classList.contains("hidden")) {
+    collapseMessage.classList.remove("hidden");
+    setTimeout(() => {
+      collapseMessage.classList.remove("visuallyhidden");
+    }, 20);
+  } else {
+    collapseMessage.classList.add("visuallyhidden");
+    collapseMessage.addEventListener("transitionend", () => {
+        collapseMessage.classList.add("hidden");
+      },
+      {
+        capture: false,
+        once: true,
+        passive: false,
+      },
+    );
+  }
+});
